4ea104d0c6cc66881b251bb10c86ccfc6dee24b3
Soham Pattanayak <117352770+yourstrulysoham@users.noreply.github.com>
2026-09-23 16:22:01 +0530

    Time: 311 ms (6.37%) | Memory: 8.6 MB (32.19%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.c
@@ -0,0 +1,10 @@
+bool match(char* s, char* p) {
+    return (s[0] == p[0]) || (p[0] == '.' && s[0]);
+}
+
+bool isMatch(char * s, char * p){
+    if (!p[0]) return !s[0];
+    if (p[1] == '*') return isMatch(s, p+2) || match(s, p) && isMatch(s+1, p);
+    if (p[0] == '.') return s[0] && isMatch(s+1, p+1);
+    return match(s, p) && isMatch(s+1, p+1);
+}
